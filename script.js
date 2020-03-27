ceddab4516cc834fd67563380379799f04831cb4
Kyler Gates <rwh.kyler@gmail.com>
2020-03-27 16:31:30 -0600

    added css

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
 let app = new Vue({
   el: '#app',
   data: {
-    joke: "<div class=\"jokes\"><img src=\"?\"></img><p>Jokes go here</p><button @click=\"generateJoke()\">Generate new Joke</button></div>"
+    max: 10,
+    min: 0,
+    num: 0,
+    generate: "Generate Joke",
+    jokes: []
   },
-  created: {
-    startapp();
+  created() {
+    this.startapp();
   },
-  methods {
-    function startapp(){
-      document.getElementById("jokes").innerHTML = joke;
+  computed: {
+    number() {
+      if (this.num > this.max) {
+        return this.max;
+      }
+      if (this.num < this.min) {
+        return this.min;
+      }
+      return this.num;
+    }
+  },
+  methods: {
+    startapp: function() {
+      while (this.jokes.length < this.number) {
+        this.jokes.push({
+          id: this.jokes.length,
+          text: "Click for Joke"
+        });
+      }
+      while (this.jokes.length > this.number) {
+        this.jokes.pop();
+      }
+    },
+    generateAllJokes: async function(){
+      for(let i = 0; i < this.number; i++){
+        await this.generateJoke(i);
+      }
+    },
+    generateJoke: async function(id) {
+      let url = "https://icanhazdadjoke.com/";
+      const response = await axios.get(url, {
+        'headers': {
+          'Accept': 'application/json'
+        }
+      });
+      this.jokes[id].text = response.data.joke;
+      this.$forceUpdate();
     }
   }
-
 });
